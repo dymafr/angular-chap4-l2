b7965d2b5f1bf1032f8f24493b5b4b0779f847bf
dymafr <erwan1309@hotmail.com>
2020-12-08 15:26:26 +0100

    Initial commit

---
+++ src/app/fruit/fruit.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-fruit",
+  templateUrl: "./fruit.component.html",
+  styleUrls: ["./fruit.component.scss"]
+})
+export class FruitComponent implements OnInit {
+  @Input() fruit: string;
+  constructor() {}
+
+  ngOnInit() {}
+}
